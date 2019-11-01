bda847a90571fcd9596ada9a3fcf980b034f90ca
jharrvis <jharrvis@gmail.com>
2019-11-01 11:25:31 +0700

    commit pertama

---
+++ application/controllers/Auth.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        //Do your magic here
+    }
+
+
+    public function index()
+    {
+        $data['title'] = "CI Login";
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email Address', 'required|trim|valid_email|is_unique[users.email]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $data['title'] = "CI Login";
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo "data";
+        }
+    }
+}
+
+/* End of file Auth.php */
